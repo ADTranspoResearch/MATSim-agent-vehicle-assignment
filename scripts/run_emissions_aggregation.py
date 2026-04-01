2544580c0ddc05e26ca194f2a0787bf3727e8d2d
ADTranspoResearch <alessandro.dapice@mail.mcgill.ca>
2026-04-01 13:08:00 -0400

    Temporary Fix due to efficiency bug

--- scripts/run_emissions_aggregation.py
+++ scripts/run_emissions_aggregation.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import _setup_path #pylint: disable=unused-import
-from matsim_vehicle_type.config import MATSIM_DIR, DATA_DIR
+from matsim_vehicle_type.config import MATSIM_DIR, DATA_DIR, OUTPUT_DIR
 from matsim_vehicle_type.emissions.aggregate_emissions import aggregate_emissions
 from matsim_vehicle_type.emissions.add_efficiency import add_efficiency
 
@@ -8,5 +8,7 @@ emissions_path = MATSIM_DIR / "emissions" / "outputemission.events.offline.xml.g
 efficiency_path = DATA_DIR / "vehicles" / "tc_improvement_all_fsa.csv"
 efficiency_df = pd.read_csv(efficiency_path)
 emissions_df = aggregate_emissions(emissions_path)
-emissions_df = add_efficiency(emissions_df, efficiency_df)
+#emissions_df = add_efficiency(emissions_df, efficiency_df)
 
+
+emissions_df.to_csv((OUTPUT_DIR / "agent_aggregated_emissions.csv"))
